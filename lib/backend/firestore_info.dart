486507a8fd11592fcd0c92624ea814471d5b8094
aditiansingh <aditiansingh@gmail.com>
2022-06-30 17:10:07 +0530

    dashboard and requirements page done

---
+++ lib/backend/firestore_info.dart
@@ -0,0 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirebaseData {
+  Future<String> addRequirement(Map<String,dynamic> requirements) async {
+    try {
+      var res = await FirebaseFirestore.instance.collection("requirements").add(
+        requirements
+      );
+      return res.id;
+    } catch (e, stackTrace) {
+      return e.toString();
+    }
+  }
+}
